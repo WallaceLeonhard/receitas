998f4cd9a67d880cd4c7df9bfb809a0ca404791e
WallaceLeonhard <wallacelw22@gmail.com>
2023-04-13 11:08:35 -0300

    feat: :sparkles: criação de rotas

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export function Home() {
+  return (
+    <View className="flex-1 bg-zinc-900 items-center justify-center">
+      <Text className="text-white font-bold text-2xl">Home!</Text>
+    </View>
+  )
+}
